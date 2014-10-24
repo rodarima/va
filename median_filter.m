7f8357eae59d994fb27b90c0b1a23a3c4b42822d
Rodrigo <rodrigo@hal>
2014-10-24 02:10:53 +0200

    Reparar índices rotos.

--- median_filter.m
+++ median_filter.m
@@ -2,14 +2,15 @@ function new_image = median_filter(P, r)
 	[n, m] = size(P);
 
 	R = uint8(zeros(n, m));
+	V = zeros(2*r+1);
 
 	% Padding
-	A = [repmat(P(1,:), r, 1); P; repmat(P(n,:), r, 1)]
-	A = [repmat(A(:,1), 1, r), A, repmat(A(:,m), 1, r)]
+	A = [repmat(P(1,:), r, 1); P; repmat(P(n,:), r, 1)];
+	A = [repmat(A(:,1), 1, r), A, repmat(A(:,m), 1, r)];
 
 	for x = 1:m,
 		for y = 1:n,
-			V = A([y-r+1:y+r+1], [x-r+1:x+r+1]);
+			V(:,:) = A([y:y+2*r], [x:x+2*r]);
 			R(y, x) = median(V(:));
 		end;
 	end;
